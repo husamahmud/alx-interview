cf899e71a03dcf340b796b01ede4871599907a11
husamahmud <husamahmud@gmail.com>
2024-06-11 19:38:16 +0300

    Add

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,10 +3,10 @@
 
 
 def rotate_2d_matrix(matrix):
-	"""function to rotate 2d matrix"""
-	for i in range(len(matrix)):
-		for j in range(i, len(matrix)):
-			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    """function to rotate 2d matrix"""
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-	for row in matrix:
-		row.reverse()
+    for row in matrix:
+        row.reverse()
